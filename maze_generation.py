ccd2b051cf1e206632e57844bb0160781dfdf8e8
GreeNoiZe <91499856+GreeNoiZe@users.noreply.github.com>
2023-09-25 12:17:58 +0200

    Add files via upload

---
+++ maze_generation.py
@@ -0,0 +1,19 @@
+from maze_backtracking import Maze
+from time import perf_counter
+
+start = perf_counter() # on initialise le compteur pour calculer le temps d'éxécution du programme
+
+maze = Maze(5,5)
+maze.backtrack(0,0)
+
+end = perf_counter() # on arrête le chronomètre
+execution_time = (end - start) # on calcule le temps d'éxécution
+
+print(maze)
+
+# On inclut le temps d'éxécution à l'affichage dans le terminal et dans le fichier
+print(f'Le labyrinthe a été généré en {execution_time} secondes')
+with open("maze.txt", "a") as f:
+    f.write('\n')
+    f.write(f'Le labyrinthe a été généré en {execution_time} secondes')
+    f.close()
